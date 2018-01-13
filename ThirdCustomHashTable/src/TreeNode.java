bbb4a32cc12e944f27ce8bb49ce8de31bf4cf2a4
Travis Thompson <travis@jaclyns-mbp.lan>
2018-01-12 20:05:11 -0800

    built out the tree classes some, renamed project

--- ThirdCustomHashTable/src/TreeNode.java
+++ ThirdCustomHashTable/src/TreeNode.java
@@ -1,4 +1,11 @@
 
 public class TreeNode {
-
+	TreeNode left;
+	TreeNode right;
+	String value;
+	
+	public TreeNode(String value) {
+		this.value = value; 
+	}
 }
+
--- ThirdCustomHashTable/src/TreeStructure.java
+++ ThirdCustomHashTable/src/TreeStructure.java
@@ -1,4 +1,13 @@
-
 public class TreeStructure {
-
+	
+//	public void build() {
+//		TreeNode root = new TreeNode("");
+//	}
+//	
+//	public void insert (String first, String last) {
+//		String firstAndLast = first.toUpperCase().concat(last.toUpperCase());
+//		if (firstAndLast.compareTo()) {
+//			
+//		}
+//	}
 }
